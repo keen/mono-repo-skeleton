2b4c3867174819c45081199f8553147eb724a3d0
Michał Czapkowicz <michal.czapkowicz@keen.io>
2022-05-23 09:39:35 +0200

    package.json test

--- prepublish.js
+++ prepublish.js
@@ -1,8 +1,3 @@
-/**
- *  This script should be fired before the process of publishing the packages.
- *  It changes file locations in package.json to compiled javascript files instead of typescript ones.
- *  It can be helpful in situation where someone doesn't use typescript compiler.
- */
 const fs = require('fs');
 
 const packagesLocation = __dirname + '/packages'
@@ -11,9 +6,9 @@ const packageNames = fs.readdirSync(packagesLocation).map(item => item);
 packageNames.map(packageName => {
   const packageLocation = `${packagesLocation}/${packageName}/package.json`
   const packageData = require(packageLocation);
-  packageData.main = packageData.main.replace('src/index.ts', 'dist/index.js');
-  packageData.module = packageData.module.replace('src/index.ts', 'dist/index.js');
-  packageData.types = packageData.types.replace('src/index.d.ts', 'typings/index.d.ts');
+  packageData.main = packageData.main.replace('dist/index.js', 'src/index.ts');
+  packageData.module = packageData.module.replace('dist/index.js', 'src/index.ts');
+  packageData.types = packageData.types.replace('typings/index.d.ts', 'src/index.d.ts');
   fs.writeFile(packageLocation, JSON.stringify(packageData, null, 2), function (err) {
     if (err) throw err;
   });
